66864e6ebc54d3d721f293de0106c37bda662d45
jiko21 <daikikojima0531@icloud.com>
2022-02-15 00:08:17 +0900

    add: some func

--- src/resolver.post.ts
+++ src/resolver.post.ts
@@ -21,8 +21,21 @@ export class PostResolver {
 
   @Query((returns) => Post, { nullable: true })
   postById(@Args('id') id: number) {
-    return this.prismaService.post.findUnique({
-      where: { id },
+    return this.prismaService.post.findFirst({
+      where: {
+        id,
+        status: 'PUBLISHED',
+      },
+    });
+  }
+
+  @Query((returns) => [Post], { nullable: true })
+  feed() {
+    return this.prismaService.post.findMany({
+      where: {
+        status: 'PUBLISHED',
+        isDelete: false,
+      },
     });
   }
 }
